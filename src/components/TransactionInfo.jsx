b6ff0be697e79d40d4c47b25743d57fc963d16e9
Mouhamadou <mouhamadoul.sarr@gmail.com>
2024-04-02 14:32:05 +0200

    styling transaction

--- src/components/TransactionInfo.jsx
+++ src/components/TransactionInfo.jsx
@@ -8,7 +8,7 @@ const TransactionInfo = ({ transaction, index }) => {
       <div
         className={`flex align-items-center box-content h-12 justify-between gap-2 py-4 ${dynamicBorder}`}
       >
-        <div className="flex align-items-center ">
+        <div className="flex align-items-center gap-1 ">
           <div className="none sm-flex w-11 content-center bg-gray-200 text-muted rounded p-3">
             <IoReceiptOutline />
           </div>
@@ -18,7 +18,7 @@ const TransactionInfo = ({ transaction, index }) => {
         </div>
         <div className="flex flex-col text-left">
           <div>
-            <span className="font-semibold">From:</span>
+            <span className="font-semibold">From: </span>
             <span className="blockInfo">
               {transaction.from.slice(0, 20)}...
             </span>
