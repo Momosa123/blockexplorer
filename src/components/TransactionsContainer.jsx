df6a9dc16efdf4f81cae268073d1866849fa213e
Mouhamadou <mouhamadoul.sarr@gmail.com>
2024-04-04 22:19:38 +0200

    correctionof transaction bug

--- src/components/TransactionsContainer.jsx
+++ src/components/TransactionsContainer.jsx
@@ -1,19 +1,22 @@
 import InfoHeader from "./InfoHeader";
 import Transaction from "./TransactionInfo";
-const TransactionsContainer = ({ blockNumber, text, transactions }) => {
+const TransactionsContainer = ({ blockNumber, transactions }) => {
   return (
     <div className="mx-auto w-11/12 rounded-lg bg-slate-50 border-solid border-1 ">
       <InfoHeader text={"Latest Transactions"} />
 
       <ul className="p-4 pt-0">
-        {transactions.map((tx, i) => (
-          <Transaction
-            key={tx.hash}
-            blockNumber={blockNumber}
-            transaction={tx}
-            index={i}
-          />
-        ))}
+        {transactions.map(
+          (tx, i) =>
+            tx && (
+              <Transaction
+                key={tx.hash}
+                blockNumber={blockNumber}
+                transaction={tx}
+                index={i}
+              />
+            )
+        )}
       </ul>
     </div>
   );
